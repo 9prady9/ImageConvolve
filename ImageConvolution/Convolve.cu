617f90bae511aa78de3d6eeaa5672a6cc5acac67
Pradeep Garigipati <pradeep.garigipati@gmail.com>
2013-11-16 18:29:23 -0500

    Added Convolution code to kernel
    The input image is copied tile by tile into the shared memory
    of for a block of threads to efficiently access it again and again.
    I have tested with 3x3 kernels so far. I believe it needs some work
    to handle color images directly. But the convolution code is working.

--- ImageConvolution/Convolve.cu
+++ ImageConvolution/Convolve.cu
@@ -3,7 +3,7 @@
 #include <qimage.h>
 #include <qdebug.h>
 
-__constant__	int	dev_ConstMemory_Kernel[81];
+__constant__	int	d_kernel[81];
 
 static int	mKernelSize;
 uchar*		dev_SourceImage;
@@ -34,25 +34,124 @@ void Kernel::setCellValue(int fRow, int fCol, int fValue)
 	krValues[fRow*temp+fCol] = fValue;
 }
 
+
+__inline__ __device__ uchar4 getRGBA(const uchar* fSource,
+						const int fImageWidth,
+						const int fImageHeight,
+						const int row,
+						const int col)
+{
+	uchar4 retVal;
+	int ii	= row<0 ? 0 : row;
+	int jj	= col>=fImageHeight ? fImageHeight-1 : col;
+	int indx= 4*(jj*fImageWidth + ii);
+	retVal.x= fSource[indx+2];
+	retVal.y= fSource[indx+1];
+	retVal.z= fSource[indx+0];
+	retVal.w= fSource[indx+3];
+	return retVal;
+}
+
+__inline__ __device__ void setRGBA(uchar* fDestination,
+						const int fImageWidth,
+						const int fImageHeight,
+						const int row,
+						const int col,
+						uint4 value)
+{
+	int ii	= row<0 ? 0 : row;
+	int jj	= col>=fImageHeight ? fImageHeight-1 : col;
+	int indx= 4*(jj*fImageWidth + ii);
+	fDestination[indx+2] = value.x;
+	fDestination[indx+1] = value.y;
+	fDestination[indx+0] = value.z;
+	fDestination[indx+3] = value.w;
+}
+
 __global__ void convolveKernel(const uchar* fSource, int fImageWidth, int fImageHeight, uchar* fDestination, int fKernelSize)
 {
-	extern __shared__ uchar canvasCache[];
-	
-	int gx = threadIdx.x + blockDim.x * blockIdx.x;
-	int gy = threadIdx.y + blockDim.y * blockIdx.y;
+	extern __shared__ uchar shared[];
 
+	int slen	= blockDim.x+2*fKernelSize;
+	int klen	= 2*fKernelSize+1;
+	int gx		= threadIdx.x + blockDim.x * blockIdx.x;
+	int gy		= threadIdx.y + blockDim.y * blockIdx.y;
+	int sidx	= 4*(threadIdx.y*slen+threadIdx.x);
+
+	uchar4 pxl	= getRGBA(fSource,fImageWidth,fImageHeight,
+						gx-fKernelSize,gy-fKernelSize);	
+
+	shared[sidx+0] = pxl.x;
+	shared[sidx+1] = pxl.y;
+	shared[sidx+2] = pxl.z;
+	shared[sidx+3] = pxl.w;
+
+	int ti	= threadIdx.x + fKernelSize;
+	int tj	= threadIdx.y + fKernelSize;	
+	int lx2	= threadIdx.x + blockDim.x;
+	int ly2	= threadIdx.y + blockDim.y;
+	int gx2	= gx + blockDim.x;
+	int gy2	= gy + blockDim.y;
+
+	if( threadIdx.x < fKernelSize ) {
+		pxl	= getRGBA(fSource,fImageWidth,fImageHeight,
+						gx2-fKernelSize,gy-fKernelSize);
+		sidx= 4*(threadIdx.y*slen+lx2);
+
+		shared[sidx+0] = pxl.x;
+		shared[sidx+1] = pxl.y;
+		shared[sidx+2] = pxl.z;
+		shared[sidx+3] = pxl.w;
+	}
+
+	if( threadIdx.y < fKernelSize ) {
+		pxl	= getRGBA(fSource,fImageWidth,fImageHeight,
+						gx-fKernelSize,gy2-fKernelSize);
+		sidx= 4*(ly2*slen+threadIdx.x);
+
+		shared[sidx+0] = pxl.x;
+		shared[sidx+1] = pxl.y;
+		shared[sidx+2] = pxl.z;
+		shared[sidx+3] = pxl.w;
+	}
+
+	if( threadIdx.x < fKernelSize && threadIdx.y < fKernelSize ) {
+		pxl	= getRGBA(fSource,fImageWidth,fImageHeight,
+						gx2-fKernelSize,gy2-fKernelSize);
+		sidx= 4*(ly2*slen+lx2);
+
+		shared[sidx+0] = pxl.x;
+		shared[sidx+1] = pxl.y;
+		shared[sidx+2] = pxl.z;
+		shared[sidx+3] = pxl.w;
+	}
+
+	__syncthreads();
+
+	// Now that the image has been read 
+	// into shared memory completely.
+	// Check for image bounds and exit
 	if( gx >= fImageWidth || gy >= fImageHeight )
 		return;
+		
+	sidx		= 4*(tj*slen+ti);
+	uchar* ptr	= shared + sidx;	
+	uint4 accum = {0,0,0,0};
 
-	int linearIdx	= 4*(gy*fImageWidth + gx);
-	int ridx		= linearIdx+2;
-	int gidx		= linearIdx+1;
-	int bidx		= linearIdx;
-	int aidx		= linearIdx+3;
-	fDestination[ridx] = fSource[ridx];
-	fDestination[gidx] = fSource[gidx];
-	fDestination[bidx] = fSource[bidx];
-	fDestination[aidx] = fSource[aidx];
+	for( int jj=-fKernelSize; jj<=fKernelSize; jj++ )
+	{
+		for( int ii=-fKernelSize; ii<=fKernelSize; ii++ )
+		{
+			int tmpidx	= 4*(jj*slen+ii);
+			int weight	= d_kernel[(fKernelSize+jj)*klen+(fKernelSize+ii)];
+			accum.x		+= weight*ptr[tmpidx+0];
+			accum.y		+= weight*ptr[tmpidx+1];
+			accum.z		+= weight*ptr[tmpidx+2];
+		}
+	}
+	accum.w	= shared[sidx+3];
+
+	setRGBA(fDestination,fImageWidth,fImageHeight,gx,gy,accum);
 }
 
 Convolve::Convolve()
@@ -70,7 +169,7 @@ Convolve::Convolve()
 void Convolve::setKernelData(const Kernel &fKernel)
 {
 	int temp			= fKernel.kr*2 + 1;
-	HANDLE_ERROR( cudaMemcpyToSymbol(dev_ConstMemory_Kernel, fKernel.krValues, temp*temp*sizeof(int)) );
+	HANDLE_ERROR( cudaMemcpyToSymbol(d_kernel, fKernel.krValues, temp*temp*sizeof(int)) );
 	mKernelSize			= fKernel.kr;
 	mIsKernelSet		= true;
 }
@@ -103,7 +202,7 @@ void Convolve::cudaConvolve()
 	initCUDA();
 	if(mIsKernelSet && mIsImageSet && mIsCUDAInit)
 	{
-		int sharedMemSize = ( mThreadsPerBlock.x*mThreadsPerBlock.y + 2*mKernelSize )*4*sizeof(uchar);
+		int sharedMemSize = (mThreadsPerBlock.y+2*mKernelSize)*(mThreadsPerBlock.x+2*mKernelSize)*4*sizeof(uchar);
 		cudaEventRecord(mStart,0);
 		convolveKernel<<<mGrid,mThreadsPerBlock,sharedMemSize>>>( dev_SourceImage, mImageWidth, mImageHeight, dev_ConvolvedImage, mKernelSize );
 		HANDLE_ERROR( cudaPeekAtLastError() );
@@ -119,7 +218,6 @@ void Convolve::cudaConvolve()
 		/* Copy back convolved image to host and show it */		
 		HANDLE_ERROR( cudaMemcpy( host_ConvolvedImage, dev_ConvolvedImage, mImageWidth*mImageHeight*4*sizeof(uchar), cudaMemcpyDeviceToHost) );
 		QImage output(host_ConvolvedImage,mImageWidth,mImageHeight, QImage::Format_ARGB32);
-		//output.save("C:\\Users\\prady\\Downloads\\output.png");
 	} else {
 		if(!mIsKernelSet)
 			printf("Prerequisite: Kernel not setup. \n");
@@ -141,30 +239,16 @@ void Convolve::initCUDA()
 {
 	HANDLE_ERROR( cudaEventCreate( &mStart ) );
 	HANDLE_ERROR( cudaEventCreate( &mStop ) );
-	/*cudaDeviceProp prop;
-	cudaGetDeviceProperties(&prop,0);
-	int MAX_SHARED_MEM = prop.sharedMemPerBlock/1024;
-	int PIXEL_LOAD_PER_DIM;		
-	for(PIXEL_LOAD_PER_DIM=7; ;++PIXEL_LOAD_PER_DIM)
-	{
-	int currentSize = (PIXEL_LOAD_PER_DIM*mThreadsPerBlock.x+2*mKernelSize)/16;
-	if(currentSize*currentSize > MAX_SHARED_MEM)
-	{
-	PIXEL_LOAD_PER_DIM--;
-	mPerThreadLoad.x = PIXEL_LOAD_PER_DIM;
-	mPerThreadLoad.y = PIXEL_LOAD_PER_DIM;
-	break;
-	}
-	}*/
+
 	mThreadsPerBlock.x = TILE_WIDTH;
 	mThreadsPerBlock.y = TILE_HEIGHT;
 	mGrid.x = ceil( mImageWidth, mThreadsPerBlock.x );
 	mGrid.y = ceil( mImageHeight, mThreadsPerBlock.y );
 
 	qDebug()<<"Threads per block "<<mThreadsPerBlock.x<<","<<mThreadsPerBlock.y;
-	//qDebug()<<"Load per Thread "<<mPerThreadLoad.x<<","<<mPerThreadLoad.y;
 	qDebug()<<"Blocks per grid "<<mGrid.x<<","<<mGrid.y;
-	//qDebug()<<"Shared memory usage : "<<((mPerThreadLoad.x*mThreadsPerBlock.x+2*mKernelSize)*(mPerThreadLoad.x*mThreadsPerBlock.x+2*mKernelSize))/256<<" KB";
+	int share  = (mThreadsPerBlock.y+2*mKernelSize)*(mThreadsPerBlock.x+2*mKernelSize)*4*sizeof(uchar);
+	qDebug()<<"Shared memory usage : "<<share<<" Bytes";
 	mIsCUDAInit= true;
 }
 
